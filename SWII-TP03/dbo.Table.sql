310f5278afe381a2c3cda018742c4108f475c89a
Anderson Bucchianico <anderson@Sirius.local>
2021-01-05 18:36:12 -0300

    Alterações

---
+++ SWII-TP03/dbo.Table.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE BL (
+	id int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	numero int NOT NULL,
+	consignee varchar(64) NOT NULL,
+	navio varchar(64) NOT NULL
+)
+
+CREATE TABLE Container
+(
+	id int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	numero int NOT NULL,
+	tipo varchar(64) NOT NULL,
+	tamanho int NOT NULL,
+	id_bl int FOREIGN KEY REFERENCES BL(id)
+)
